7adf16c2fd10cf1154895d6f09fbab79fd451216
Yana <56785142+etoxotic@users.noreply.github.com>
2021-12-03 04:07:38 +0300

    draw ui

--- Assets/Scripts/UI/MainMenu/UIMainMenuController.cs
+++ Assets/Scripts/UI/MainMenu/UIMainMenuController.cs
@@ -39,7 +39,8 @@ public class UIMainMenuController : MonoBehaviour
     private void ExitButtonHandler()
     {
         progressData.Clear();
-        // UnityEditor.EditorApplication.isPlaying = false;
+        if(UnityEditor.EditorApplication.isPlaying)
+            UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
